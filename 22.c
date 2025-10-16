951d985231105a6622ad350d66fbb6e27e0e2445
Alejandro Pachas <alejandropachas1@gmail.com>
2025-10-15 23:54:01 -0500

    22.c

--- 22.c
+++ 22.c
@@ -1,30 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+void backtracking(char ***res, int start, int end, int *returnSize, int *n, char *str, int *cap)
+{
+  if(start < *n){
+    str[start + end] = '(';
+    backtracking(res, start + 1, end, returnSize, n, str, cap);
+  }
+  if(end < start){
+    str[start + end] = ')';
+    backtracking(res, start, end + 1, returnSize, n, str, cap);
+  }
+  if((end + start ) == 2 *(*n)){
+    if(*returnSize >= *cap){
+      *cap *=2;
+      *res =  (char **)realloc(*res, sizeof(char *) * (*cap));
+    }
+    (*res)[*returnSize] = strdup(str);
+    (*returnSize)++;
+    return;
+  }
 
-typedef struct {
-  int top;
-  char **arr;
-} Stack;
+}
 
 char** generateParenthesis(int n, int* returnSize) {
- int start = 0, closing = 0;
- char str[2 * n + 1];
- char ** res = malloc(sizeof(char*) * (*returnSize));
- for (int i = 0; i < n; i++){
-
-   if( closing < start) {
-   } else {
-     
-    
-   }
- }
-  
+ int start = 0, close = 0, cap = 8;
+ *returnSize = 0;
+ char * str = calloc( n * 2 + 1, sizeof(char));
+ char ** res = (char **)malloc(sizeof(char*) * cap);
+ backtracking(&res, start, close, returnSize, &n, str,&cap);
+ return res;
 }
 
 int main()
 {
-  int a = 5;
-
-  generateParenthesis(3, &a);
+  int a = 0;
+  int b = 8;
+  char ** res = generateParenthesis(b, &a);
+  for ( int i = 0; i < a; i++)
+    printf("res: %s\n", res[i]);
   return 0;
 }
