c3c8197d7de44cb7d316dfac298b3aecb9135200
Alejandro Pachas <alejandropachas1@gmail.com>
2025-11-29 19:57:55 -0500

    853.c

--- 853.c
+++ 853.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+typedef struct{ 
+  int position;
+  int speed;
+}Car;
 int comp (const void *a, const void*b) {
-  return *(int*)b - *(int*)a;
+  return ((Car*)b)->position - ((Car*)a)->position ;
 }
 int carFleet(int target, int* position, int positionSize, int* speed, int speedSize) {
   /*
@@ -17,26 +20,31 @@ int carFleet(int target, int* position, int positionSize, int* speed, int speedS
   4 1 => 5
   5 1 => 6    =>  5, 1 => 6
   */
+  Car *cars = malloc(sizeof(Car) * positionSize);
+  for (int i = 0; i < positionSize; i++) {
+    cars[i].position = position[i];
+    cars[i].speed = speed[i];
+  }
   // sorting 
-  qsort(position, positionSize, sizeof(int), comp);
+  qsort(cars, positionSize, sizeof(Car), comp);
   int t, head = 0;
-  int *fleet = calloc(speedSize, sizeof(int));
-  fleet[head] = speed[0];
+  float *fleet = calloc(speedSize + 1, sizeof(float));
+  
   //filling up the fleets
-  for (int i = positionSize - 1; i >= 0; i--) {
-    t = (target - position[i]) / speed[i];
-    if (t <= fleet[head]){
-      fleet[++head] = speed[i];
+  for (int i = 0; i < positionSize; i++) {
+    fleet[++head] = (float)(target - cars[i].position) / (float)cars[i].speed;
+    if( head >= 2 && (fleet[head - 1]  >= fleet[head]) ) {
+      fleet[head--] = 0;
     }
   }
   return head;
 }
 
 int main(){
-  int position[] = {0,2,4};
-  int speed[] = {4,2,1};
-  int target = 100;
-  printf("res: %d\n", carFleet(target, position, 3, speed, 3));
+  int position[] = {10,8,0,5,3};
+  int speed[] = {2,4,1,1,3};
+  int target = 12;
+  printf("res: %d\n", carFleet(target, position, 5, speed, 5));
 
 return 0;
 }
