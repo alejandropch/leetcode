ec9c47a044febbda82ec856856e29b68fb1fc218
Alejandro Pachas <alejandropachas1@gmail.com>
2025-12-07 19:41:25 -0500

    704.c

---
+++ 704.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int search(int* nums, int numsSize, int target) {
+  int l = 0, r = numsSize - 1, mid = numsSize/2; 
+  while ( l <= r ) {
+    if(target == nums[mid])
+      return mid;
+    if(target < nums[mid])
+      r = mid - 1;
+    if(target > nums[mid])
+      l = mid + 1;
+
+    mid = l + (l - r) / 2;
+  } 
+  return -1;
+}
+
+int main(){
+  int nums[] = {-1,0,3,5,9,12};
+  int r = search(nums, sizeof(nums)/sizeof(int), 9);
+  printf("res: %d\n", r);
+  return 0;
+  
+}
